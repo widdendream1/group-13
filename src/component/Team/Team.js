787a157e9ad56c487936b1c47df08cb5cb70411f
widdendream1 <ishitagupta1012@gmail.com>
2022-09-13 11:33:37 +0530

    qwww

--- src/component/Team/Team.js
+++ src/component/Team/Team.js
@@ -21,8 +21,8 @@ const Team = () => {
                             <div class="card">
                                 <div class="card-body text-center">
                   
-                                    <p> <Image class="img-fluid"   filename={`1.jpeg`}
-                                   alt="1"/></p> <h4 class="card-title text-muted">VERSHA KUMARI</h4>
+                                    <p> <Image class="img-fluid"   filename={`1.jpg`}
+                                   alt="1"/></p> <h4 class="card-title text-muted">TANYA GOSWAMI</h4>
                                    <p className="text-center text-muted"><i>Web developer </i></p>
                                     <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-plus"></i></a>
                                 </div>
@@ -31,7 +31,7 @@ const Team = () => {
                         <div class="backside">
                             <div class="card">
                                 <div class="card-body text-center mt-4 ">
-                                    <h4 class="card-title">VERSHA KUMARI</h4>
+                                    <h4 class="card-title">TANYA GOSWAMI</h4>
                                     <p class="card-text align-items-center">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
 </p>
